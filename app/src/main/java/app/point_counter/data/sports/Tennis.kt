9a4a692d015983b9aaef9bd6a2911d7d94d8f148
François Liraud <fliraud.edu@gmail.com>
2025-09-14 16:19:43 +0200

    Added Tiebreak [Error diff 2 in games ex:6-5]

--- app/src/main/java/app/point_counter/data/sports/Tennis.kt
+++ app/src/main/java/app/point_counter/data/sports/Tennis.kt
@@ -21,37 +21,60 @@ class Tennis : Sport() {
         tieBreak2Diff = true,
     )
 
+    var tieBreak = false
+
     override fun addPointToPlayer(player: Int) {
-        if (player == 1) {
-            when (score.player1Pts) {
-                0, 15 -> score.addPts(1, 15)
-                30 -> score.addPts(1, 10) // 50 MEANS AV !!!
-                40 -> {
-                    if (score.player2Pts == 50)
-                        score.addPts(2, -10) // Both go back to 40
-                    else
-                        score.addPts(1, 10) // AV to Plr 1
+        if (!tieBreak) {
+            if (player == 1) {
+                when (score.player1Pts) {
+                    0, 15 -> score.addPts(1, 15)
+                    30 -> score.addPts(1, 10) // 50 MEANS AV !!!
+                    40 -> {
+                        if (score.player2Pts == 50)
+                            score.addPts(2, -10) // Both go back to 40
+                        else
+                            score.addPts(1, 10) // AV to Plr 1
+                    }
+                    50 ->
+                    {
+                        score.resetPts()
+                        addGamesToPlayer(1)
+                    }
                 }
-                50 ->
-                {
-                    score.resetPts()
-                    addGamesToPlayer(1)
+            } else if (player == 2) {
+                when (score.player2Pts) {
+                    0, 15 -> score.addPts(2, 15)
+                    30 -> score.addPts(2, 10) // 50 MEANS AV !!!
+                    40 -> {
+                        if (score.player1Pts == 50)
+                            score.addPts(1, -10) // Both go back to 40
+                        else
+                            score.addPts(2, 10) // AV to Plr 2
+                    }
+                    50 -> {
+                        score.resetPts()
+                        addGamesToPlayer(2)
+                    }
                 }
             }
+        } else {
+            addPointToPlayerTieBreak(player)
+        }
+    }
+
+    fun addPointToPlayerTieBreak(player: Int) {
+        score.addPts(player)
+        if (player == 1) {
+            if (score.player1Pts >= 7 && score.player1Pts - score.player2Pts >= 2) {
+                score.addGames(1)
+                score.resetPts()
+                tieBreak = false
+            }
         } else if (player == 2) {
-            when (score.player2Pts) {
-                0, 15 -> score.addPts(2, 15)
-                30 -> score.addPts(2, 10) // 50 MEANS AV !!!
-                40 -> {
-                    if (score.player1Pts == 50)
-                        score.addPts(1, -10) // Both go back to 40
-                    else
-                        score.addPts(2, 10) // AV to Plr 2
-                }
-                50 -> {
-                    score.resetPts()
-                    addGamesToPlayer(2)
-                }
+            if (score.player2Pts >= 7 && score.player2Pts - score.player1Pts >= 2) {
+                score.addGames(2)
+                score.resetPts()
+                tieBreak = false
             }
         }
     }
@@ -95,12 +118,15 @@ class Tennis : Sport() {
                     score.addSet(2)
             }
         }
+        if (score.player1Games == 6 && score.player2Games == 6) {
+            tieBreak = true
+        }
     }
 
     override fun checkWin(): Int {
-        if (score.player1Sets == rules.setsToWin)
+        if (score.player1Sets == rules.setsToWin && score.player1Games - score.player2Games >= 2)
             return 1
-        else if (score.player2Sets == rules.setsToWin)
+        else if (score.player2Sets == rules.setsToWin && score.player2Games - score.player1Games >= 2)
             return 2
         else
             return 0
